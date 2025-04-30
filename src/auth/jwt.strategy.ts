52f4698b35a07b067f9c216e6619e63962b9b59f
ESardorbe <egamberdiyevsardorbek549@gmail.com>
2025-04-30 10:49:17 +0500

    jwt.strategy.ts fix

--- src/auth/jwt.strategy.ts
+++ src/auth/jwt.strategy.ts
@@ -13,7 +13,7 @@ export class JwtStrategy extends PassportStrategy(Strategy, "jwt") {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
       ignoreExpiration: false,
-      secretOrKey: process.env.JWT_SECRET_KEY,
+      secretOrKey: 'texnosardor',
     })
   }
 
